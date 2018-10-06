41e87b8d9416d09fd1b9a9612b9594b356067139
Jan Altensen (Stricted) <info@stricted.net>
2018-10-06 22:16:06 +0200

    Revert "replace deprecated kernel header dependency"
    This reverts commit 704ce32c6f8712dacfd52277c6a9a39682536002.
    
    Change-Id: I7971f2c57819ae659598e04d063ccc47d704b0a1

--- libgscaler/Android.mk
+++ libgscaler/Android.mk
@@ -44,7 +44,7 @@ LOCAL_C_INCLUDES := \
 	$(TOP)/hardware/samsung_slsi-cm/exynos/libmpp
 
 LOCAL_ADDITIONAL_DEPENDENCIES := \
-	INSTALLED_KERNEL_HEADERS
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SRC_FILES := \
 	libgscaler_obj.cpp \
