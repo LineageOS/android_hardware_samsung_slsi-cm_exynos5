0b325a6d59c90cdd125e5c475f4e7af5fd47e611
Stricted <info@stricted.net>
2018-10-19 03:26:58 +0200

    exynos5: Use generated kernel headers
    Change-Id: I2af6401145bb7d0a452c88e0ae57c35cabad048d

--- libscaler/Android.mk
+++ libscaler/Android.mk
@@ -20,13 +20,11 @@ include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libexynosutils libexynosv4l2
 
 LOCAL_C_INCLUDES := \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 	$(LOCAL_PATH)/../include \
 	$(TOP)/hardware/samsung_slsi-cm/exynos/include \
 	$(TOP)/hardware/samsung_slsi-cm/exynos/libexynosutils
 
-LOCAL_ADDITIONAL_DEPENDENCIES := \
-	INSTALLED_KERNEL_HEADERS
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
 LOCAL_SRC_FILES := libscaler.cpp
 
